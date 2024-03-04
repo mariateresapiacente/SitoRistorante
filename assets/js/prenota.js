f8f57573a42f1a6bf616c05614f598bad8def4d3
mariateresapiacente <mariateresa.piacente@icloud.com>
2024-03-04 13:20:35 +0100

    update

--- assets/js/prenota.js
+++ assets/js/prenota.js
@@ -1,27 +1,28 @@
-var prenotazioni = {};
-function verificaDisponibilita(date, time) {
+function verificaDisponibilita() {
+    var data = document.getElementById("datePicker").value;
+    var orario = document.getElementById("timeSlot").value;
+    var numeroPersone = document.getElementById("numberOfPeople").value;
+
     // Definisci gli orari disponibili per ciascun giorno della settimana
     const orariDisponibili = {
         Lunedi: ["12:00", "13:00", "18:00", "19:00", "20:00", "21:00", "22:00"],
         Martedi: [],
         Mercoledi: ["12:00", "13:00", "18:00", "19:00", "20:00", "21:00", "22:00"],
         Giovedi: ["12:00", "13:00", "18:00", "19:00", "20:00", "21:00", "22:00"],
-        Venerdi: [], // Aggiungi gli orari disponibili per ciascun giorno
+        Venerdi: [],
         Sabato: ["12:00", "13:00", "18:00", "19:00", "20:00", "21:00", "22:00"],
         Domenica: ["12:00", "13:00", "18:00", "19:00", "20:00", "21:00", "22:00"]
     };
 
     // Ottieni il giorno della settimana dalla data fornita
-    const giorno = new Date(date).toLocaleDateString('en-US', { weekday: 'long' });
+    const giorno = new Date(data).toLocaleDateString('it-ITA', { weekday: 'long' });
 
     // Controlla se l'orario è presente negli orari disponibili per il giorno specificato
-    if (orariDisponibili[giorno].includes(time)) {
-        return true; // Orario disponibile
+    if (orariDisponibili[giorno].includes(orario)) {
+        // Orario disponibile
+        alert("Prenotazione confermata per il " + data + " alle " + orario + " per " + numberOfPeople + " persone.");
     } else {
-        return false; // Orario non disponibile
+        // Orario non disponibile
+        alert("L'orario selezionato non è disponibile per la prenotazione. Si prega di selezionare un altro orario.");
     }
 }
-
-// Esempi di utilizzo della funzione
-console.log(verificaDisponibilita("2024-03-10", "12:00")); // Output: true
-console.log(verificaDisponibilita("2024-03-10", "14:00")); // Output: false
